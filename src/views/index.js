d4eb31d2c0a2026da9c619628a4c085e9e060aca
Nikolas Savvidis <nicos.savidis@gmail.com>
2018-07-29 06:54:21 +0200

    add some additional design flavor

--- src/views/index.js
+++ src/views/index.js
@@ -19,8 +19,12 @@ export default {
                ])
             ]),
             m('.hero.row.justify-content-center.align-items-center', [
-               m('.col-auto', [
-                  m("svg.hero__logo", m('use[xlink:href="icons.svg#slipper-logo"]'))
+               m('.col-12.col-sm-11.col-lg-7.align-self-center.text-center', [
+                  m("svg.hero__logo", m('use[xlink:href="icons.svg#slipper-logo"]')),
+                  m('.hero__caption.d-block.pt-4.pb-2', "A document generator for Brixtol Textiles. Slipper generates document sheets that can be printed by integrating paramaters from the URL into views."),
+                  m("svg.icon.mt-4", [
+                     m('use[xlink:href="icons.svg#print"]')
+                  ])
                ])
             ])
          ])
