8149cc4abab9124f07ecc2d29504b0f0bd662f7f
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-10-01 00:17:23 +0200

    fix path

--- scripts/strip.js
+++ scripts/strip.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require('fs');
 const { resolve } = require('path');
-const file = readFileSync(resolve(process.cwd(), 'test.liquid')).toString();
+const file = readFileSync(resolve(process.cwd(), 'email/parameters.liquid')).toString();
 const strip = file.replace(/[\n]+|[\s]{2,}/g, '');
 
 console.log(strip);
